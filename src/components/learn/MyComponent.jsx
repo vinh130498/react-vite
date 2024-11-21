54a2023700bfce41b34420692c0774d92fd3a058
Nguyen Doan Vinh <nguyendoanvinh1998@gmail.com>
2024-11-21 10:00:55 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -6,11 +6,23 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const studyReact = "vinh"; //string
+    // const studyReact = 25; //number
+    // const studyReact = true; //boolean
+    // const studyReact = undefined;
+    // const studyReact = null;
+    const studyReact = [1, 2, 3];
+    // const studyReact = {
+    //     name: "vinh",
+    //     age: 26
+    // }
+
     return (
         <>
-            <div> vinh & study react update</div >
+            <div> {JSON.stringify(studyReact)} & study react update</div >
+            <div>{console.log("VINH")}</div>
             <div className="child"
-                style={{ borderRadius: "10px" }}
+                style={{ borderRadius: "10px" }} // dấu nháy đôi thứ nhất tượng trưng cho sử dụng JS trong HTML, còn thứ hai tượng trưng cho object
             >child</div>
         </>
     );
