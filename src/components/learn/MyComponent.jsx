7a69a696cc807af6241935711fa2d25ef9d2344e
Nguyen Doan Vinh <nguyendoanvinh1998@gmail.com>
2024-11-21 09:02:48 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,18 @@
 // () => { }
 // component = html + css + js
+// JSX: 1 parent
+// fragment <></>
+
+import './style.css';
+
 const MyComponent = () => {
     return (
-        <div> vinh & study react update</div>
+        <>
+            <div> vinh & study react update</div >
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
     );
 }
 
