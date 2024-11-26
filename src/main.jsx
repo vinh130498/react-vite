5bff54ac8d211866529ea1d0ed6a1480b08629fa
Nguyen Doan Vinh <nguyendoanvinh1998@gmail.com>
2024-11-26 09:28:14 +0700

    video 46

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import UserPage from './pages/user.jsx';
+import ProductPage from './pages/product.jsx';
+import './styles/global.css';
 
 const router = createBrowserRouter([
   {
@@ -14,19 +18,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <div>login page</div>
+    element: <LoginPage />
   },
   {
     path: "/register",
-    element: <div>register page</div>
+    element: <RegisterPage />
   },
   {
     path: "/users",
-    element: <div>users page</div>
+    element: <UserPage />
   },
   {
     path: "/products",
-    element: <div>products page</div>
+    element: <ProductPage />
   },
 ]);
 
